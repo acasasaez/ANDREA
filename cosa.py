3c152df6193a00e26006d10d48041f11967e060b
acasasaez <acasasae@myuax.com>
2021-12-15 10:19:29 +0100

    Create cosa.py

---
+++ cosa.py
@@ -0,0 +1,15 @@
+import random
+def hola ():
+    n = 5
+    tablero  = []
+    for i in range (5):
+        for j in range (i):
+            i ="#"
+        
+                
+                    
+
+            tablero.append(i)
+            
+        print (i)
+hola()
